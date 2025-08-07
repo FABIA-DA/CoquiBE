2f0c268bbf277a1881e99890d7f8f89b0d3ccbb7
LukasPerger <l.perger@HTBLALeonding.onmicrosoft.com>
2025-08-07 18:37:39 +0200

    first tts server

---
+++ main.py
@@ -0,0 +1,22 @@
+﻿import torch
+from TTS.api import TTS
+from fastapi import FastAPI, status
+from pydantic import BaseModel
+
+class ConversionRequest(BaseModel):
+    text: str
+    fileName: str
+
+app = FastAPI()
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+tts = TTS("tts_models/de/css10/vits-neon").to(device)
+
+@app.post("/convert")
+def convert(request: ConversionRequest, status_code=status.HTTP_204_NO_CONTENT):
+    tts.tts_to_file(
+        text=request.text,
+        file_path=f"out/{request.fileName}.wav"
+    )
+    return
